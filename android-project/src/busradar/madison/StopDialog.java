154fbbe7561826e9c5b22228e7acd50f73b70c5c
X <fioobar@example.com>
2014-02-12 23:28:10 -0800

    Added license

--- android-project/src/busradar/madison/StopDialog.java
+++ android-project/src/busradar/madison/StopDialog.java
@@ -1,3 +1,17 @@
+// Copyright (C) 2010 Aleksandr Dobkin, Michael Choi, and Christopher Mills.
+// 
+// This file is part of BusRadar <https://github.com/orgs/busradar/>.
+// 
+// BusRadar is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+// 
+// BusRadar is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
 package busradar.madison;
 
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
